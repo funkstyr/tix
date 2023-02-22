d346fb22067f6bc55cb9ba8756650b3e047b2b28
funkstyr <30911142+funkstyr@users.noreply.github.com>
2023-02-21 20:54:13 -0500

    wip: add ticket router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,5 +1,6 @@
 import { authRouter } from "./router/auth";
 import { postRouter } from "./router/post";
+import { ticketRouter } from "./router/ticket";
 import { userRouter } from "./router/user";
 import { createTRPCRouter } from "./trpc";
 
@@ -7,6 +8,7 @@ export const appRouter = createTRPCRouter({
   post: postRouter,
   auth: authRouter,
   user: userRouter,
+  ticket: ticketRouter,
 });
 
 // export type definition of API
---
+++ packages/api/src/router/ticket.ts
@@ -0,0 +1,70 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { protectedProcedure } from "./../trpc";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+/**
+ * /api/tickets - GET
+ * /api/tickets/:id - GET
+ * /api/tickets - POST, title: string, price: string
+ * /api/tickets/:id - PUT, title: string, price string
+ */
+
+export const ticketRouter = createTRPCRouter({
+  all: publicProcedure.query(({ ctx }) => {
+    // TODO: dont return tickets that have an order status of created, payment, or completed
+    return ctx.prisma.ticket.findMany({ orderBy: { id: "desc" } });
+  }),
+  byId: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.prisma.ticket.findFirst({ where: { id: input } });
+  }),
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+        price: z.number().min(1),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+
+      return ctx.prisma.ticket.create({ data: { ...input, userId } });
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1).optional(),
+        price: z.number().min(1).optional(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+
+      const ticket = await prisma.ticket.findUnique({
+        where: { id: input },
+      });
+
+      if (ticket.userId !== userId) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+        });
+      }
+      return ctx.prisma.ticket.update({ data: input });
+    }),
+  delete: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
+    const userId = ctx.session.user.id;
+
+    const ticket = await prisma.ticket.findUnique({
+      where: { id: input },
+    });
+
+    if (ticket.userId !== userId) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+      });
+    }
+
+    return ctx.prisma.ticket.delete({ where: { id: input } });
+  }),
+});
