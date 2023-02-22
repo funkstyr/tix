00bfe3c888f47c63e970ff0c81a54c06bfd98d27
funkstyr <funkstyr@gmail.com>
2023-02-21 21:18:48 -0500

    fix: add ctx to missing prisma operations

--- packages/api/src/router/ticket.ts
+++ packages/api/src/router/ticket.ts
@@ -33,32 +33,47 @@ export const ticketRouter = createTRPCRouter({
   update: protectedProcedure
     .input(
       z.object({
+        id: z.string(),
         name: z.string().min(1).optional(),
         price: z.number().min(1).optional(),
       }),
     )
     .mutation(async ({ ctx, input }) => {
       const userId = ctx.session.user.id;
+      const { id, ...data } = input;
 
-      const ticket = await prisma.ticket.findUnique({
-        where: { id: input },
+      const ticket = await ctx.prisma.ticket.findUnique({
+        where: { id },
       });
+
+      if (!ticket) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+        });
+      }
 
       if (ticket.userId !== userId) {
         throw new TRPCError({
           code: "UNAUTHORIZED",
         });
       }
-      return ctx.prisma.ticket.update({ data: input });
+
+      return ctx.prisma.ticket.update({ where: { id }, data });
     }),
   delete: protectedProcedure
     .input(z.string())
     .mutation(async ({ ctx, input }) => {
       const userId = ctx.session.user.id;
 
-      const ticket = await prisma.ticket.findUnique({
+      const ticket = await ctx.prisma.ticket.findUnique({
         where: { id: input },
       });
+
+      if (!ticket) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+        });
+      }
 
       if (ticket.userId !== userId) {
         throw new TRPCError({
--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -8,20 +8,6 @@ const userNameSchema = z.string().min(4).max(25);
 const passwordSchema = z.string().min(6);
 
 export const userRouter = createTRPCRouter({
-  //   all: publicProcedure.query(({ ctx }) => {
-  //     return ctx.prisma.post.findMany({ orderBy: { id: "desc" } });
-  //   }),
-  //   byId: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-  //     return ctx.prisma.post.findFirst({ where: { id: input } });
-  //   }),
-  //   create: publicProcedure
-  //     .input(z.object({ title: z.string().min(1), content: z.string().min(1) }))
-  //     .mutation(({ ctx, input }) => {
-  //       return ctx.prisma.post.create({ data: input });
-  //     }),
-  //   delete: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
-  //     return ctx.prisma.post.delete({ where: { id: input } });
-  //   }),
   current: protectedProcedure.mutation(({ ctx }) => {}),
   login: publicProcedure
     .input(
