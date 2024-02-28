3905b81a91f29cb5cd57801c7d918092485b40b6
funkstyr <funkstyr@gmail.com>
2024-02-27 19:35:26 -0500

    feat: add ui package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,6 +2,8 @@ import type { CxOptions } from "class-variance-authority";
 import { cx } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
-const cn = (...inputs: CxOptions) => twMerge(cx(inputs));
+function cn(...inputs: CxOptions) {
+  return twMerge(cx(inputs));
+}
 
 export { cn };
