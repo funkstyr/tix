30906840328d56e792d8441b9e07d34cafe0e094
Michael Funk <30911142+funkstyr@users.noreply.github.com>
2026-05-24 07:30:26 -0700

    ci: add stateful services (#99)

--- infra/pulumi/index.ts
+++ infra/pulumi/index.ts
@@ -1,3 +1,33 @@
+import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 
+import { StatefulInfra } from "./stateful-infra.ts";
+
 export const stack = pulumi.getStack();
+
+const config = new pulumi.Config();
+const desiredNamespace = config.get("namespace") ?? "tix";
+const postgresPassword = config.requireSecret("postgresPassword");
+
+const namespace = new k8s.core.v1.Namespace("tix-namespace", {
+  metadata: { name: desiredNamespace },
+});
+
+const infra = new StatefulInfra(
+  "tix",
+  {
+    namespace: namespace.metadata.name,
+    postgres: {
+      password: postgresPassword,
+      database: "tix",
+      storage: "1Gi",
+    },
+    nats: { storage: "1Gi" },
+  },
+  { dependsOn: namespace },
+);
+
+export const namespaceName = namespace.metadata.name;
+export const postgresService = infra.postgres.metadata.name;
+export const natsService = infra.nats.metadata.name;
+export const redisService = infra.redis.metadata.name;
